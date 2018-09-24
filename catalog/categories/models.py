9766fcb8a3c4ad9c2047191851d9c5ac0e1d6fd4
Alejandro De Jesus <alejandro.dejesus93@gmail.com>
2018-09-23 23:12:27 -0400

    feat: Create Category list and details page
    Resolves: #12

--- catalog/categories/models.py
+++ catalog/categories/models.py
@@ -1,4 +1,4 @@
-from .app import db
+from catalog import db
 
 
 class Category(db.Model):
--- catalog/categories/views.py
+++ catalog/categories/views.py
@@ -1,9 +1,20 @@
-from flask import render_template, Blueprint
-
+from flask import Blueprint, render_template, abort
+from .models import Category
 
 categories_bp = Blueprint('categories', __name__, template_folder='templates')
 
 
 @categories_bp.route('/')
 def index():
-    return render_template('index.html')
+    categories = Category.query.all()
+    return render_template('index.html', categories=categories)
+
+
+@categories_bp.route('/<int:cid>')
+def details(cid):
+    category = Category.query.get(cid)
+
+    if category:
+        return render_template('details.html', category=category)
+    else:
+        abort(404)
