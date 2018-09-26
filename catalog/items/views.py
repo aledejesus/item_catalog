08484fa90a8259236ccd695f3b0c00a65a277106
Alejandro De Jesus <alejandro.dejesus93@gmail.com>
2018-09-25 21:53:40 -0400

    feat: Create add Item page
    References: #24

--- catalog/items/views.py
+++ catalog/items/views.py
@@ -1,6 +1,12 @@
-from flask import Blueprint, render_template, abort, jsonify
+from copy import copy
+from flask import (
+    Blueprint, render_template, abort, jsonify, request,
+    redirect, url_for)
 from sqlalchemy.orm import joinedload
+
+from catalog import db
 from .models import Item
+from .forms import ItemForm
 
 items_bp = Blueprint('items', __name__)
 
@@ -23,3 +29,31 @@ def details_json(iid):
         return jsonify({'item': item.serialize()})
     else:
         abort(404)
+
+
+@items_bp.route('/add/', methods=('GET', 'POST'))
+def add():
+    try:
+        cid = int(request.args.get('cid'))
+    except Exception as e:
+        print(e)
+        return redirect(url_for('categories.index'))
+        # TODO: FLASH MESSAGE STATING PROBLEM
+
+    form = ItemForm()
+    form.category_id.data = cid
+
+    if form.validate_on_submit():
+        try:
+            item_props = copy(form.data)
+            del item_props['csrf_token']
+            item = Item(**item_props)
+            db.session.add(item)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+            # TODO: FLASH MESSAGE STATING PROBLEM
+        else:
+            # TODO: FLASH CREATION SUCCESS MESSAGE
+            return redirect(url_for('items.details', iid=item.id))
+    return render_template('items/add.html', form=form)
