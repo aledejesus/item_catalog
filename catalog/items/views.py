427f89a00d39f35600c818b02f0854a75fb164fc
Alejandro De Jesus <alejandro.dejesus93@gmail.com>
2018-09-26 22:24:21 -0400

    feat: Add item name validation to /items/add
    This was made to give a more detailed message when a user is
    trying to create an item with the name of an existent item.
    
    Resolves: #24

--- catalog/items/views.py
+++ catalog/items/views.py
@@ -1,10 +1,11 @@
 from copy import copy
 from flask import (
     Blueprint, render_template, abort, jsonify, request,
-    redirect, url_for)
+    redirect, url_for, flash)
 from sqlalchemy.orm import joinedload
 
 from catalog import db
+from catalog.categories.models import Category
 from .models import Item
 from .forms import ItemForm
 
@@ -33,27 +34,41 @@ def details_json(iid):
 
 @items_bp.route('/add/', methods=('GET', 'POST'))
 def add():
+    cid = ''
     try:
         cid = int(request.args.get('cid'))
+        Category.query.filter_by(id=cid).one()
     except Exception as e:
         print(e)
+        flash("Invalid category id ({})".format(cid), 'error')
         return redirect(url_for('categories.index'))
-        # TODO: FLASH MESSAGE STATING PROBLEM
 
     form = ItemForm()
     form.category_id.data = cid
 
     if form.validate_on_submit():
+        item_props = copy(form.data)
+        del item_props['csrf_token']
+
+        item = Item.query.filter_by(name=item_props['name']).first()
+        if item:
+            flash("name: Item with name '{}' already exists".format(
+                item_props['name']))
+            return render_template('items/add.html', form=form)
+
         try:
-            item_props = copy(form.data)
-            del item_props['csrf_token']
             item = Item(**item_props)
             db.session.add(item)
             db.session.commit()
         except Exception as e:
             print(e)
-            # TODO: FLASH MESSAGE STATING PROBLEM
+            flash("An error ocurred while creating the item", 'error')
         else:
-            # TODO: FLASH CREATION SUCCESS MESSAGE
+            flash("Item created successfully", 'info')
             return redirect(url_for('items.details', iid=item.id))
+    else:
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash("{}: {}".format(field, error), 'error')
+
     return render_template('items/add.html', form=form)
