0d17455e44765de743252db5347ea6a31dd62546
Alejandro De Jesus <alejandro.dejesus93@gmail.com>
2018-09-27 21:01:12 -0400

    feat: Create edit Item page
    Also add a link to the edit item page in the Item details page
    for easier access.
    
    Resolves: #25

--- catalog/items/views.py
+++ catalog/items/views.py
@@ -2,6 +2,7 @@ from copy import copy
 from flask import (
     Blueprint, render_template, abort, jsonify, request,
     redirect, url_for, flash)
+from sqlalchemy import and_, not_
 from sqlalchemy.orm import joinedload
 
 from catalog import db
@@ -72,3 +73,52 @@ def add():
                 flash("{}: {}".format(field, error), 'error')
 
     return render_template('items/add.html', form=form)
+
+
+@items_bp.route('/edit/<int:iid>', methods=('GET', 'POST'))
+def edit(iid):
+    try:
+        item = Item.query.filter_by(id=iid).one()
+    except Exception as e:
+        print(e)
+        flash("Item with id ({}) does not exist".format(iid), 'error')
+        return redirect(url_for('categories.index'))
+
+    form = ItemForm(obj=item)
+
+    if form.validate_on_submit():
+        # check name duplicity
+        same_name_item = Item.query.filter(
+            and_(Item.name == form.name.data,
+                 not_(Item.id == item.id))).first()
+        if same_name_item:
+            flash("name: Item with name '{}' already exists".format(
+                form.name.data), 'error')
+            return render_template('items/edit.html', form=form)
+
+        # validate category exists
+        category = Category.query.get(form.category_id.data)
+        if not category:
+            flash("category_id: Category with id ({}) does not exist".format(
+                form.category_id.data), 'error')
+            return render_template('items/edit.html', form=form)
+
+        # modify item
+        try:
+            for k, v in form.data.items():
+                if hasattr(item, k):
+                    setattr(item, k, v)
+            db.session.add(item)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+            flash("An error ocurred while modifying the item", 'error')
+        else:
+            flash("Item modified successfully", 'info')
+            return redirect(url_for('items.details', iid=item.id))
+    else:
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash("{}: {}".format(field, error), 'error')
+
+    return render_template('items/edit.html', form=form)
