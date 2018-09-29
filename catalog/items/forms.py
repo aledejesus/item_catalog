9c26526159a71dd411e0488303908e452207d582
Alejandro De Jesus <alejandro.dejesus93@gmail.com>
2018-09-28 23:34:32 -0400

    feat: Create delete Item page
    Only a view was created as no information needs to be visualized
    when deleting an item. DeleteItemForm was created to provide
    CSRF protection to delete form. It was included in the Item
    details page.
    
    Resolves: #26

--- catalog/items/forms.py
+++ catalog/items/forms.py
@@ -1,12 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms import StringField, IntegerField, TextField, HiddenField
+from wtforms.validators import ValidationError, DataRequired, NumberRange
 
 
 class ItemForm(FlaskForm):
+    """ Form used for Item creation and edition """
     name = StringField('Name', validators=[DataRequired()])
     description = TextField('Description', validators=[DataRequired()])
     quantity = IntegerField(
         'Quantity', validators=[DataRequired(), NumberRange(min=0)])
     category_id = IntegerField(
         'Category', validators=[DataRequired(), NumberRange(min=1)])
+
+
+class DeleteItemForm(FlaskForm):
+    """
+    Form used to delete item. It is mainly used to provide CSRF
+    protection. Later on, this form will be modified to include
+    item existence check.
+    """
+    action = HiddenField(default='delete_item', validators=[DataRequired()])
+
+    def validate_action(form, field):
+        if field.data != 'delete_item':
+            raise ValidationError("This value should not be tampered with")
